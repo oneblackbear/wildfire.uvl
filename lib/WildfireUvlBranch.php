eb8846a5187b9ecaa3dcb91cb273fafaf8b5f0c9
charlesmarshall <cm56marshall@googlemail.com>
2011-12-15 12:47:29 +0000

    extend wildfirecontent, change some fields etc

--- lib/WildfireUvlBranch.php
+++ lib/WildfireUvlBranch.php
@@ -1,10 +1,18 @@
 <?
-class WildfireUvlBranch extends WildfireModel{
-  
+class WildfireUvlBranch extends WildfireContent{
+
   public function setup(){
+    parent::setup();
     $this->define("code", "CharField", array('required'=>true)); //a unique ref - if dealer is getting a multi branch import, this code needs to match
+    $this->define("status", "BooleanField"); //simplified version of status
     
+    //remove the date_start / date_end
+    unset($this->columns['date_start'], $this->columns['date_end']);
   }
-  
+
+  public function scope_live(){
+    return $this->filter("status", 1)->order("sort ASC, title ASC");
+  }
+
 }
 ?>
