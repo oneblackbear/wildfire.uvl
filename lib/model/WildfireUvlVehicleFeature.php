e7e18b1c2a3562d898fc6f30d03217556c5abc43
charlesmarshall <cm56marshall@googlemail.com>
2011-12-15 18:03:37 +0000

    feature, fuel and transmission models for dynamic adding

---
+++ lib/model/WildfireUvlVehicleFeature.php
@@ -0,0 +1,13 @@
+<?
+class WildfireUvlVehicleFeature extends WaxModel{
+  
+  public function setup(){
+    $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
+    $this->define("searchable", "Boolean", array('scaffold'=>true, 'required'=>true));
+  }
+  
+  public function before_save(){
+    if(!$this->title) $this->title = "TITLE";
+  }
+}
+?>
