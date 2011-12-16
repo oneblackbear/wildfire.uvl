f1e0a968c1dcd6bff910d3a1aa64acafd4f2e7ee
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 10:25:16 +0000

    starting the branch controller

---
+++ lib/controller/CMSAdminUvlbranchController.php
@@ -0,0 +1,9 @@
+<?
+class CMSAdminUvlbranchController extends CMSAdminUvlBaseController{
+  public $module_name = "uvlbranch";												
+  public $model_class = 'WildfireUvlBranch';
+  public $display_name = "Dealerships";
+  public $dashboard = false;
+  
+}
+?>
