267c8f8d01ff3cadb72ebdf2d176a27f46661055
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 11:19:24 +0000

    base vehicle controller

---
+++ lib/controller/CMSAdminUvlvehicleController.php
@@ -0,0 +1,9 @@
+<?
+class CMSAdminUvlvehicleController extends CMSAdminUvlController{
+  public $module_name = "uvlvehicle";												
+  public $model_class = 'WildfireUvlVehicle';
+  public $display_name = "Vehicles";
+  public $dashboard = false;
+  
+}
+?>
