adeee18c07f7b06441a97e9c903a8d721a5d3a53
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 11:37:15 +0000

    whitespace

--- lib/model/WildfireUvlVehicleFuel.php
+++ lib/model/WildfireUvlVehicleFuel.php
@@ -1,11 +1,11 @@
 <?
 class WildfireUvlVehicleFuel extends WaxModel{
-  
+
   public function setup(){
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
-    $this->define("searchable", "BooleanField", array('scaffold'=>true, 'required'=>true));    
+    $this->define("searchable", "BooleanField", array('scaffold'=>true, 'required'=>true));
   }
-  
+
   public function before_save(){
     if(!$this->title) $this->title = "TITLE";
   }
