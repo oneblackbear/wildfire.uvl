18a3488e717697f5ab17745fc17125ef89f38a7e
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 10:38:39 +0000

    doh, missed the field off

--- lib/model/WildfireUvlVehicleFuel.php
+++ lib/model/WildfireUvlVehicleFuel.php
@@ -3,7 +3,7 @@ class WildfireUvlVehicleFuel extends WaxModel{
   
   public function setup(){
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
-    $this->define("searchable", "Boolean", array('scaffold'=>true, 'required'=>true));    
+    $this->define("searchable", "BooleanField", array('scaffold'=>true, 'required'=>true));    
   }
   
   public function before_save(){
