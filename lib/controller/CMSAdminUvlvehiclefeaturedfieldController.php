64353d54af95143c2ecb4525b7ab512ec8b8aa45
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 11:38:07 +0000

    featured fields

---
+++ lib/controller/CMSAdminUvlvehiclefeaturedfieldController.php
@@ -0,0 +1,9 @@
+<?
+class CMSAdminUvlvehiclefeaturedfieldController extends AdminComponent{
+  public $module_name = "uvlvehiclefeaturedfield";												
+  public $model_class = 'WildfireUvlVehicleFeaturedField';
+  public $display_name = "Vehicle Featured Field";
+  public $dashboard = false;
+  
+}
+?>
--- setup.php
+++ setup.php
@@ -3,4 +3,5 @@
 CMSApplication::register_module("uvlbranch", array("display_name"=>"Dealerships", "link"=>"/admin/uvlbranch/"));
 CMSApplication::register_module("uvlvehicle", array("display_name"=>"Vehicles", "link"=>"/admin/uvlvehicle/"));
 CMSApplication::register_module("uvlvehiclefeature", array("display_name"=>"Vehicle Features", "link"=>"/admin/uvlvehiclefeature/"));
+CMSApplication::register_module("uvlvehiclefeaturedfield", array("display_name"=>"Vehicle Featured Fields", "link"=>"/admin/uvlvehiclefeaturedfield/"));
 ?>
