95850267b4ed1292ecca1fb030ecabc52cc684b2
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 11:19:37 +0000

    register it

--- setup.php
+++ setup.php
@@ -1,4 +1,5 @@
 <?
 
 CMSApplication::register_module("uvlbranch", array("display_name"=>"Dealerships", "link"=>"/admin/uvlbranch/"));
+CMSApplication::register_module("uvlvehicle", array("display_name"=>"Vehicles", "link"=>"/admin/uvlvehicle/"));
 ?>
