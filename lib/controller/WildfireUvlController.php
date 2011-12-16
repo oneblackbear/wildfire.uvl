d53fe8d7366c43c1a2b1111ca21ee19dcae8a0f4
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 12:34:13 +0000

    skel actions of what will be used

--- lib/controller/WildfireUvlController.php
+++ lib/controller/WildfireUvlController.php
@@ -1,15 +1,36 @@
 <?
 class WildfireUvlController extends ApplicationController{
-  
+
   //pushing back to the stack
   public function controller_global(){
     WaxEvent::add("cms.cms_stack_set", function(){
       $obj = WaxEvent::data();
       array_unshift($obj->cms_stack, $obj->controller);
-    });    
+    });
     parent::controller_global();
   }
-  
-  
+
+
+  /**
+   * handles the display, filtering, search etc of vehicles
+   */
+
+  public function __vehicle_listing(){}
+  //small on used on the listing
+  public function __vehicle_summary(){}
+  //main one
+  public function __vehicle(){}    
+
+  protected function __vehicle_filters(){}
+
+  /**
+   * handle dealership lookups
+   */
+  public function __dealership_listings(){}
+  public function __dealership_summary(){}
+  public function __dealership(){}
+
+  public function __dealership_filters(){}
+
 }
 ?>
