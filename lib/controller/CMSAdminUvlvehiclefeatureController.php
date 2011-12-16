9fe56e705c28892e3e5a95838ea19217a82ef6c2
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 11:37:47 +0000

    vehicle features

---
+++ lib/controller/CMSAdminUvlvehiclefeatureController.php
@@ -0,0 +1,9 @@
+<?
+class CMSAdminUvlvehiclefeatureController extends AdminComponent{
+  public $module_name = "uvlvehiclefeature";												
+  public $model_class = 'WildfireUvlVehicleFeature';
+  public $display_name = "Vehicle Features";
+  public $dashboard = false;
+  
+}
+?>
--- setup.php
+++ setup.php
@@ -2,4 +2,5 @@
 
 CMSApplication::register_module("uvlbranch", array("display_name"=>"Dealerships", "link"=>"/admin/uvlbranch/"));
 CMSApplication::register_module("uvlvehicle", array("display_name"=>"Vehicles", "link"=>"/admin/uvlvehicle/"));
+CMSApplication::register_module("uvlvehiclefeature", array("display_name"=>"Vehicle Features", "link"=>"/admin/uvlvehiclefeature/"));
 ?>
