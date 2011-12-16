3fc743b00f766c7290f518198087c891327df386
charlesmarshall <cm56marshall@googlemail.com>
2011-12-16 15:16:46 +0000

    rename

--- lib/model/WildfireUvlVehicleField.php
+++ lib/model/WildfireUvlVehicleField.php
@@ -1,9 +1,10 @@
 <?
-class WildfireUvlVehicleFeaturedField extends WaxModel{
+class WildfireUvlVehicleField extends WaxModel{
   
   public function setup(){
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
     $this->define("column_name", "CharField", array('scaffold'=>true, 'required'=>true));
+    $this->define("featured", "BooleanField", array('scaffold'=>true, 'required'=>true));
   }
   
   public function before_save(){
