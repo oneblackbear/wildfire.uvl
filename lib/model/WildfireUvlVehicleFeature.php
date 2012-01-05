e58adc06151b026f191dfa0255f97df762a670b0
charlesmarshall <cm56marshall@googlemail.com>
2012-01-05 19:17:18 +0000

    notes about the model

--- lib/model/WildfireUvlVehicleFeature.php
+++ lib/model/WildfireUvlVehicleFeature.php
@@ -1,9 +1,11 @@
 <?
+/**
+ * this is for the extra random data like cd changer, central locking etc that come back in a big lump
+ */
 class WildfireUvlVehicleFeature extends WaxModel{
   
   public function setup(){
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
-    $this->define("searchable", "BooleanField", array('scaffold'=>true, 'required'=>true));
   }
   
   public function before_save(){
--- lib/model/WildfireUvlVehicleField.php
+++ lib/model/WildfireUvlVehicleField.php
@@ -1,4 +1,8 @@
 <?
+/**
+ * this is to decide what is shown on the actual advert, and which should be the main featured values
+ * - ie (CO<sub>2</sub> emissions, co2, 1) would make co2 column value one of the main items shown
+ */
 class WildfireUvlVehicleField extends WaxModel{
   
   public function setup(){
