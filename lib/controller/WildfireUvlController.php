82345b537800e7bfdbbb3d356188d4c78b44cc22
charlesmarshall <cm56marshall@googlemail.com>
2012-01-05 18:46:56 +0000

    startin work on vehicle lists

--- lib/controller/WildfireUvlController.php
+++ lib/controller/WildfireUvlController.php
@@ -2,6 +2,7 @@
 class WildfireUvlController extends ApplicationController{
   
   public $paginate_dealership_list = false;
+  public $paginate_vehicle_list = true;
   
   //pushing back to the stack
   public function controller_global(){
@@ -18,13 +19,23 @@ class WildfireUvlController extends ApplicationController{
    * handles the display, filtering, search etc of vehicles
    */
 
-  public function __vehicle_listing(){}
+  public function __vehicle_listing(){
+    $model = new $this->cms_content_class($this->cms_live_scope);
+    $model = $this->__vehicle_filters($model);
+    if($this->paginate_vehicle_list){
+      if(!$this->this_page = Request::param('page')) $this->this_page = 1;
+      $this->vehicles = $model->page($this->this_page, $this->per_page);
+    }else $this->vehicles = $model->all();
+
+  }
   //small on used on the listing
   public function __vehicle_summary(){}
   //main one
   public function __vehicle(){}    
 
-  protected function __vehicle_filters(){}
+  protected function __vehicle_filters($model){
+    return $model;
+  }
 
   /**
    * handle dealership lookups
