3625d1f4951c6a5ed3434325801bdc8ea8343070
charlesmarshall <cm56marshall@googlemail.com>
2012-01-05 19:22:15 +0000

    new admin controllers for the search field picker

---
+++ lib/controller/CMSAdminUvlvehiclesearchoptionsController.php
@@ -0,0 +1,9 @@
+<?
+class CMSAdminUvlvehiclesearchoptionsController extends AdminComponent{
+  public $module_name = "uvlvehiclesearchoptions";												
+  public $model_class = 'WildfireUvlVehicleSearchField';
+  public $display_name = "Search Options";
+  public $dashboard = false;
+  
+}
+?>
---
+++ resources/app/controller/admin/AdminUvlvehiclesearchoptionsController.php
@@ -0,0 +1,3 @@
+<?php
+class AdminUvlvehiclesearchoptionsController extends CMSAdminUvlvehiclesearchoptionsController{}
+?>
