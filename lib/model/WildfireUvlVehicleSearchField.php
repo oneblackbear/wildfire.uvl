b6dea38e42aa950ce8a14d3582c0386e97a116f8
charlesmarshall <cm56marshall@googlemail.com>
2012-01-05 21:57:12 +0000

    extra column to handle the inc. step on the range sliders

--- lib/model/WildfireUvlVehicleSearchField.php
+++ lib/model/WildfireUvlVehicleSearchField.php
@@ -8,6 +8,7 @@ class WildfireUvlVehicleSearchField extends WaxModel{
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
     $this->define("column_name", "CharField", array('scaffold'=>true, 'required'=>true));
     $this->define("search_type", "CharField", array('widget'=>'SelectInput', 'choices'=>array('multiselect'=>'multiple choice tick boxes', 'select'=>'single choice drop down', 'range'=>'min -> max range slider') ));
+    $this->define("increment", "FloatField", array('maxlength'=>"6,2", 'label'=>'Increment (range type only)'));
   }
   
   public function before_save(){
