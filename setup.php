9ff966d8a07998ec65f05b9b61bdd4ec47e441c2
charlesmarshall <cm56marshall@googlemail.com>
2012-01-05 19:46:23 +0000

    tidy the setup options and add in the new controllers

--- setup.php
+++ setup.php
@@ -1,9 +1,14 @@
 <?
 
-CMSApplication::register_module("uvlbranch", array("display_name"=>"Dealerships", "link"=>"/admin/uvlbranch/"));
-CMSApplication::register_module("uvlvehicle", array("display_name"=>"Vehicles", "link"=>"/admin/uvlvehicle/"));
-CMSApplication::register_module("uvlvehiclefeature", array("display_name"=>"Vehicle Features", "link"=>"/admin/uvlvehiclefeature/"));
-CMSApplication::register_module("uvlvehiclefeaturedfield", array("display_name"=>"Vehicle Fields", "link"=>"/admin/uvlvehiclefeaturedfield/"));
+CMSApplication::register_module("uvlbranch", array("display_name"=>"Dealerships", "link"=>"/admin/uvlbranch/", 'split'=>true));
+
+CMSApplication::register_module("uvlvehicletransmission", array("display_name"=>"Transmissions", "link"=>"/admin/uvlvehicletransmission/"));
 CMSApplication::register_module("uvlvehiclefuel", array("display_name"=>"Fuel Types", "link"=>"/admin/uvlvehiclefuel/"));
-CMSApplication::register_module("uvlvehicletransmission", array("display_name"=>"Tranmissions", "link"=>"/admin/uvlvehicletransmission/"));
+CMSApplication::register_module("uvlvehiclefeaturedfield", array("display_name"=>"Advert Fields", "link"=>"/admin/uvlvehiclefeaturedfield/"));
+CMSApplication::register_module("uvlvehiclesearchoptions", array("display_name"=>"Search Options", "link"=>"/admin/uvlvehiclesearchoptions/"));
+CMSApplication::register_module("uvlvehiclefeature", array("display_name"=>"Extra Vehicle Features", "link"=>"/admin/uvlvehiclefeature/"));
+
+CMSApplication::register_module("uvlvehicle", array("display_name"=>"Vehicles", "link"=>"/admin/uvlvehicle/", 'split'=>true));
+
+
 ?>
