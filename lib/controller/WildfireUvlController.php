30fd72485a027817eecde901db9372af4707a783
charlesmarshall <cm56marshall@googlemail.com>
2012-01-06 10:53:34 +0000

    updated comments

--- lib/controller/WildfireUvlController.php
+++ lib/controller/WildfireUvlController.php
@@ -17,24 +17,33 @@ class WildfireUvlController extends ApplicationController{
 
   }
 
+  /**
+   * VEHICLES
+   */
+   
+  //small on used on the listing
+  public function __vehicle_summary(){}
+  //main one - view of the actual vehicle
+  public function __vehicle(){}
 
   /**
-   * handles the display, filtering, search etc of vehicles
+   * handles the display, filtering, search etc of vehicles - main partial
    */
-
   public function __vehicle_listing(){
     $model = new $this->vehicle_class($this->cms_live_scope);
     if(!$this->vehicle_filters) $this->vehicle_filters = Request::param('vehicle');
     if(!$this->vehicle_sort) $this->vehicle_sort = Request::param('sort');
     $model = $this->__vehicle_sort( $this->__vehicle_filters($model, $this->vehicle_filters), $this->vehicle_sort);
-    
+
     if($this->paginate_vehicle_list){
       if(!$this->this_page = Request::param('page')) $this->this_page = 1;
       $this->vehicles = $model->page($this->this_page, $this->per_page);
     }else $this->vehicles = $model->all();
 
   }
-  //find min - max values for search options, custom search fields etc
+  /**
+   * find min - max values for search options, custom search fields etc, partial is used in __vehicle_listing
+   */
   public function __vehicle_search_options($cache=true, $return=false){
     //this could be a very slow query, lots of db look ups, so add in some caching
     if($cache && ($cached = $this->__uvl_cache("__vehicle_search_options"))) $search_options = $cached;
@@ -52,7 +61,9 @@ class WildfireUvlController extends ApplicationController{
     $this->search_options = $search_options;
     if($return) return $this->search_options;
   }
-  //this is a range column, so we just look for the min & max values on the db
+  /**
+   * this is a range column, so we just look for the min & max values on the db, called by __vehicle_search_options
+   */
   protected function __vehicle_search_range_values($model, $column){
     //find min & max of this column
     $wax_model = new WaxModel;
@@ -60,7 +71,9 @@ class WildfireUvlController extends ApplicationController{
     $res = $wax_model->query($sql)->fetchAll();
     return array('min'=>$res[0]['minval'], 'max'=>$res[0]['maxval']);
   }
-  //this goes over a join, so we have multiple options to look at
+  /**
+   * this goes over a join, so we have multiple options to look at - called by __vehicle_search_options
+   */
   protected function __vehicle_search_select_options($model, $join_name){
     $options = array();
     //join
@@ -75,13 +88,9 @@ class WildfireUvlController extends ApplicationController{
     }
     return $options;
   }
-
-
-  //small on used on the listing
-  public function __vehicle_summary(){}
-  //main one
-  public function __vehicle(){}
-
+  /**
+   * take the passed in filter data, work out what data type it is, call its matching function which updates the model with filters
+   */
   protected function __vehicle_filters($model, $filters){
     //go over the filters, compare to the search and based on what type they are run code to update the model filters
     $search_options = $this->__vehicle_search_options(true, true);
@@ -91,39 +100,46 @@ class WildfireUvlController extends ApplicationController{
     }
     return $model;
   }
-
-  protected function __vehicle_sort($model, $posted){
-    if(!$posted && ($sort = new WildfireUvlVehicleSortField($this->cms_content_scope)) && ($first = $sort->first())) $posted = $first->primval;
-    if(($info = new WildfireUvlVehicleSortField($posted)) && $info->primval) $model = $model->order($info->column_name." ".$info->direction);
-    return $model;
-  }
-
+  /**
+   * the most basic filter, ranges are between 2 values, so set the >= / <= on the model for the relevant column
+   */
   protected function __vehicle_filter_range($model, $col, $values){
     if($values['min']) $model = $model->filter($col, $values['min'], ">=");
     if($values['max']) $model = $model->filter($col, $values['max'], "<=");
     return $model;
   }
-
+  /**
+   * more complicated, this could be either a many to many, a regular column or a foreign key
+   * - for joins, its complicated, so call another function which will lookup the join table etc
+   * - foreign key will need to find the real column name for the db filter by joining table & primary key from the other side
+   * - otherwise its just a straight $col = $values
+   */
   protected function __vehicle_filter_multiselect($model, $col,$values){
     //many to many
-    if($model->columns[$col][1]['target_model'] && $model->columns[$col][0] == "ManyToManyField") return $this->__vehicle_join_filter($model, $col, $values);
+    if($values && $model->columns[$col][1]['target_model'] && $model->columns[$col][0] == "ManyToManyField") return $this->__vehicle_filter_join($model, $col, $values);
+    elseif($values && $model->columns[$col][0] == "ForeignKey" && ($mc = $model->columns[$col][1]['target_model']) && ($m = new $mc) ) return $model->filter($mc->table."_".$mc->primary_key, $values);
     //otherise assume its a filter on a foreign key or a group col like body_style etc
     elseif($values) return $model->filter($col, $values);
     else return $model;
   }
-  //looks at the join table
-  protected function __vehicle_join_filter($model, $col, $values){
+  /**
+   * finds all the id's from the model class that are joined to $col item that is present in $values array
+   * - if $model is the vehicle, $col is "transmission" and $values is array(1,2) this will find all vehicles
+   *   which are joined to transmission type 1 or 2
+   */
+  protected function __vehicle_filter_join($model, $col, $values){
+    //find the target table
     $target_class = $model->columns[$col][1]['target_model'];
     $target = new $target_class;
     $target_table = $target->table;
     $original_table = $model->table;
-
+    //the keys for each side on the table
     $target_key = $target->table."_".$target->primary_key;
     $original_key = $model->table."_".$model->primary_key;
-
+    //figure what the join table is called
     if($target_table < $original_table) $table = $target_table."_".$original_table;
     else $table = $original_table."_".$target_table;
-
+    
     $ids = implode(",", $values);
     $query_model = new WaxModel;
     $filter_ids = array(0);
@@ -131,11 +147,25 @@ class WildfireUvlController extends ApplicationController{
 
     return $model->filter($model->primary_key, $filter_ids);
   }
-
+  /**
+   * the normal select dropdown should only be a column or a foreign key, so pass along to multiple select version which can handle it
+   */
   protected function __vehicle_filter_select($model, $col, $value){
     return $this->__vehicle_filter_multiselect($model, $col, $value);
   }
-
+  /**
+   * check the post data for sort value, if none is found than apply the first sort field from the db (called in __vehicle_listing)
+   */
+  protected function __vehicle_sort($model, $posted){
+    if(!$posted && ($sort = new WildfireUvlVehicleSortField($this->cms_content_scope)) && ($first = $sort->first())) $posted = $first->primval;
+    if(($info = new WildfireUvlVehicleSortField($posted)) && $info->primval) $model = $model->order($info->column_name." ".$info->direction);
+    return $model;
+  }
+  
+  /**
+   * DEALERSHIPS
+   */
+   
   /**
    * handle dealership lookups
    */
