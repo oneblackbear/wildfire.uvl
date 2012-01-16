1c6745d773e789b226fbc6d9ed3bb6ea648f836c
charlesmarshall <cm56marshall@googlemail.com>
2012-01-16 12:59:18 +0000

    search field has a new type, compound search (make -> model)

--- lib/model/WildfireUvlVehicleSearchField.php
+++ lib/model/WildfireUvlVehicleSearchField.php
@@ -7,8 +7,9 @@ class WildfireUvlVehicleSearchField extends WaxModel{
   public function setup(){
     $this->define("title", "CharField", array('scaffold'=>true, 'required'=>true));
     $this->define("column_name", "CharField", array('scaffold'=>true, 'required'=>true));
-    $this->define("search_type", "CharField", array('widget'=>'SelectInput', 'choices'=>array('multiselect'=>'multiple choice tick boxes', 'select'=>'single choice drop down', 'range'=>'min -> max range slider') ));
+    $this->define("search_type", "CharField", array('widget'=>'SelectInput', 'choices'=>array('multiselect'=>'multiple choice tick boxes', 'select'=>'single choice drop down', 'range'=>'min -> max range slider', 'compound'=>'Compounded drop downs (make -> model)') ));
     $this->define("increment", "FloatField", array('maxlength'=>"6,2", 'label'=>'Increment (range type only)'));
+    $this->define("position", "CharField", array('maxlength'=>"10", 'label'=>'Compound position', 'widget'=>'SelectInput', 'choices'=>array(''=>"--", 'start'=>'start', 'end'=>'end')));    
   }
   
   public function before_save(){
