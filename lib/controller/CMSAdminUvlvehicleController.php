f4d627aee946179b0de10b349a8769fd0ab12848
charlesmarshall <cm56marshall@googlemail.com>
2012-05-14 17:50:51 +0100

    tree restriction

--- lib/controller/CMSAdminUvlvehicleController.php
+++ lib/controller/CMSAdminUvlvehicleController.php
@@ -1,11 +1,19 @@
 <?
 class CMSAdminUvlvehicleController extends CMSAdminUvlController{
-  public $module_name = "uvlvehicle";												
+  public $module_name = "uvlvehicle";
   public $model_class = 'WildfireUvlVehicle';
   public $display_name = "Vehicles";
   public $dashboard = false;
   public $file_tags = array('gallery image');
-  
-  
+  public static $restricted_tree = false;
+
+  // public function events(){
+  //   parent::events();
+  //   WaxEvent::add("cms.model.filters", function(){
+  //     $obj = WaxEvent::data();
+  //     if($branches = $obj->current_user->)
+  //   });
+  // }
+
 }
 ?>
