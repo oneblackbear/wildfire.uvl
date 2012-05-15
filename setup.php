c6650028897c7ab930632ed58720c60c254a2da0
charlesmarshall <cm56marshall@googlemail.com>
2012-05-15 14:56:11 +0100

    event hook for the joining of a dealer to a branch and creation of the branch to match with the dealership

--- setup.php
+++ setup.php
@@ -24,6 +24,38 @@ if(defined("DEALERS")){
     $obj = WaxEvent::data();
     $obj->define("dealers", "ManyToManyField", array('target_model'=>"Dealer", 'group'=>'relationships'));
   });
+  //create branch when saving and the id is set
+  WaxEvent::add("Dealer.branch_creation", function(){
+    $dealer = WaxEvent::data();
+    if($dealer->primval && $dealer->title){
+      $branch = new WildfireUvlBranch;
+      //if the join already exists
+      if(($branches = $dealer->branches) && $branches->count()) $branch = $branches->first();
+      //copy over the data from dealer to the new branch
+      $details = array(
+                  'title'         => $dealer->title,
+                  'code'          => $dealer->client_id,
+                  'status'        => $dealer->status,
+                  'address_line_1'=> $dealer->address_line_1,
+                  'address_line_2'=> $dealer->address_line_2,
+                  'address_line_3'=> $dealer->address_line_3,
+                  'address_line_4'=> $dealer->city,
+                  'address_line_5'=> $dealer->county,
+                  'postcode'      => $dealer->postal_code,
+                  'telephone'     => $dealer->telephone,
+                  'email'         => $dealer->email,
+                  'opening_hours' => $dealer->opening_times,
+                  'lat'           => $dealer->lat,
+                  'lng'           => $dealer->lng,
+                  'date_start'    => $dealer->date_start,
+                  'date_end'      => $dealer->date_end
+                  );
+      if($saved = $branch->update_attributes($details)){
+        $branch->dealers = $dealer;
+      }
+    }
+  });
+
 }
 
 
