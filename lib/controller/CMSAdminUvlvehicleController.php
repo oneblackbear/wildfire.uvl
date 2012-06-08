856a73349ceb139524147b3bed6392bc55f350c8
Sheldon Els <sheldon.els@gmail.com>
2012-06-08 12:18:01 +0100

    filter vehicle list for dealer users

--- lib/controller/CMSAdminUvlvehicleController.php
+++ lib/controller/CMSAdminUvlvehicleController.php
@@ -7,6 +7,25 @@ class CMSAdminUvlvehicleController extends CMSAdminUvlController{
   public $file_tags = array('gallery image');
   public static $restricted_tree = false;
 
+  public function events(){
+    parent::events();
+    if(defined("DEALERS")){
+      //only show vehicles for that dealer when logged in as a dealer-based user
+      WaxEvent::add("cms.model.filters", function(){
+        $controller = WaxEvent::data();
+        if($dealer = $controller->current_user->dealer) $controller->model->filter("dealer_id", $dealer->id);
+      });
 
+      //set dealer based on user creating the new vehicle
+      WaxEvent::add("cms.save.after", function(){
+        $controller = WaxEvent::data();
+        $model = $controller->model;
+        if(($dealer = $controller->current_user->dealer) && !$model->dealer){
+          $model->dealer = $dealer;
+          $model->save();
+        }
+      });
+    }
+  }
 }
 ?>
