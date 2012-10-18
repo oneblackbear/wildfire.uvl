731e08c087d3d5f7701cc607a5c179cfda0ab6f5
Sheldon Els <sheldon.els@gmail.com>
2012-10-18 20:02:01 +0100

    redirect when done

--- lib/controller/CMSAdminUvlvehicleController.php
+++ lib/controller/CMSAdminUvlvehicleController.php
@@ -29,6 +29,7 @@ class CMSAdminUvlvehicleController extends CMSAdminUvlController{
   }
 
   public function import(){
+    print_r($this->controller); exit;
     if($dealer = $this->current_user->dealer){
       $import = new WildfireUvlImport(array(
         "import_dir" => WAX_ROOT."tmp/used_import/$dealer->client_id",
@@ -36,6 +37,7 @@ class CMSAdminUvlvehicleController extends CMSAdminUvlController{
       ));
       $import->import_all();
     }
+    $this->redirect_to("/$this->controller");
   }
 }
 ?>
