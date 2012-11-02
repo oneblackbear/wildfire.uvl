bea6cbb6401182cc7741f4b82705cfc0e4371dde
Ross Riley <riley.ross@gmail.com>
2012-11-02 11:31:03 +0000

    improved caching of model list

--- lib/model/WildfireUvlVehicleList.php
+++ lib/model/WildfireUvlVehicleList.php
@@ -1,5 +1,8 @@
 <?php
 class WildfireUvlVehicleList extends WaxModel{
+  
+  public static $models = false;
+  public static $manufacturers = false;
 
   public function setup(){
     $this->define("manufacturer", "CharField", array());
@@ -9,22 +12,26 @@ class WildfireUvlVehicleList extends WaxModel{
 
 
   public static function find_manufacturers() {
+    if(self::$manufacturers) return self::$manufacturers;
     $model = new WildfireUvlVehicleList;
     $results = $model->group("manufacturer")->all();
     $manufacturers = array();
     foreach($results as $res) $manufacturers[]=$res->manufacturer;
+    self::$manufacturers = $manufacturers;
     return $manufacturers;
   }
   
   public static function find_models($make = false, $prefix = true) {
+    if(!$make && self::$models) return self::$models;
     $model = new WildfireUvlVehicleList;
     if($make) $model->filter("manufacturer",$make);
     $results = $model->all();
-    $models = array();
+    $models = array(""=>"-- SELECT --");
     foreach($results as $res) {
       if($prefix) $models[(string)$res->model]=$res->manufacturer." - ".$res->model;
       else $models[(string)$res->model]=$res->model;
     }
+    if(!$make) self::$models = $models;
     return $models;
   }
   
