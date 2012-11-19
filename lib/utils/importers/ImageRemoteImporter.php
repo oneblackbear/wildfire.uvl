80c70e23eb0f721e8b883351c73a65f86f99221c
Ross Riley <riley.ross@gmail.com>
2012-11-19 12:27:48 +0000

    moved into utils

--- lib/utils/importers/ImageRemoteImporter.php
+++ lib/utils/importers/ImageRemoteImporter.php
@@ -19,30 +19,5 @@ class ImageRemoteImporter extends ImageFileImporter {
     $this->options = $options;
   }
   
-  public function parse() {
-    $model = new WildfireMedia;
-    if(!$this->options['title'])          $this->options['title'] = basename($this->url);
-    if(!$this->options['file_type'])      $this->options['file_type'] = File::detect_mime($file);
-    if(!$this->options['media_class'])    $this->options['media_class'] = 'WildfireDiskFile';
-    
-    if($this->destination) $this->options['uploaded_location'] = $this->destination;
-    else $this->options['uploaded_location'] = PUBLIC_DIR. "files/".date("Y-m-W")."/";
-    
-    $this->options['hash'] = hash_hmac('sha1', $this->data, md5($this->data));
-    if(!$options['ext']) $options['ext'] = (substr(strrchr($this->file,'.'),1));
-
-    if($saved = $model->update_attributes($options)){
-      $obj = new WildfireDiskFile;
-      $obj->set($saved);
-    }
-    return $saved;
-  }
-
-  
-  
-  public function data() {
-    return $this->data;
-  }
-  
   
 }
