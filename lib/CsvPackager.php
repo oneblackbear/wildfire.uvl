281b06f5a98eec25180bed6906c83ac58badeb45
Sheldon Els <sheldon.els@gmail.com>
2012-11-23 10:53:11 +0000

    ftp upload

--- lib/CsvPackager.php
+++ lib/CsvPackager.php
@@ -92,12 +92,21 @@ class CsvPackager{
 
   public static function package($format){
     if(!($mapping = self::$field_mapping[$format])) throw WaxException("format required");
-    $image_export_path = CACHE_DIR."/export/$format/".date("c")."/";
+    $image_export_path = CACHE_DIR."export/$format/".date("c")."/";
     $export_name = "oneblackbear-".date("dmY")."-DMS14";
     mkdir($image_export_path, 0777, true);
     $data = partial("uvl/__vehicle_listing", array("paginate_vehicles_list"=>false, "image_export_path"=>$image_export_path, "field_mapping"=>$mapping), "csv");
     file_put_contents($image_export_path.$export_name.".txt", $data);
-    exec("cd ".$image_export_path." && zip -j ".$export_name.".zip *");
+    exec("cd ".$image_export_path." && zip -jm $export_name.zip *");
+    $conf = Config::get("uvl_export");
+    self::ftp($image_export_path, $conf[$format]);
     return 1;
+  }
+
+  public static function ftp($folder, $ftp_details){
+    $conn_id = ftp_connect($ftp_details['host']);
+    $login_result = ftp_login($conn_id, $ftp_details['username'], $ftp_details['password']);
+    foreach(glob("$folder*") as $file) ftp_put($conn_id, basename($file), $file, FTP_BINARY);
+    ftp_close($conn_id);
   }
 }
